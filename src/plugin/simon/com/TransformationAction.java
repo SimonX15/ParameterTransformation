b8cf5be69dd16e0500367baeaf561a9a0073f28a
xiaowei <SimonX15@163.com>
2017-12-07 10:29:43 +0800

    Initial commit

---
+++ src/plugin/simon/com/TransformationAction.java
@@ -0,0 +1,12 @@
+package plugin.simon.com;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class TransformationAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        
+    }
+}
